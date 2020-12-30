446fe9090f2d5251677f932b4d02f272f0e90ebc
h <h@hs-MacBook-Pro.lan>
2020-12-30 10:20:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+
+https://cloud.google.com/speech-to-text/docs/sync-recognize
+
+
+```
+python3 -m venv myvenv
+source myvenv/bin/activate
+pip install -r requirements.txt
+```
+
+
+
+```
+pip install --upgrade google-cloud-speech
+
+```
---
+++ glp_speech2text.py
@@ -0,0 +1,32 @@
+import os
+import io
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/h/kindom/Kaden-API-a97f83b977be.json"
+
+from google.cloud import speech
+client = speech.SpeechClient()
+print("client", client)
+
+# gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+# audio = speech.RecognitionAudio(uri=gcs_uri)
+
+# speech_file= "data/demo.wav"
+speech_file= "data/0.wav"
+
+with io.open(speech_file, "rb") as audio_file:
+    content = audio_file.read()
+
+audio = speech.RecognitionAudio(content=content)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="en-US",
+)
+
+response = client.recognize(config=config, audio=audio)
+
+for result in response.results:
+    print("Transcript: {}".format(result.alternatives[0].transcript))
+
+print("done")
